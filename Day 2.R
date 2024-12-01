d8fdb97a91383627a4e087ef27d2fbd3c93fc916
NafeesBinReza <nafeesbinreza.sau@gmail.com>
2024-12-01 11:12:43 +0600

    the rest

---
+++ Day 2.R
@@ -0,0 +1,71 @@
+sum(12, 10)
+
+#Function name = sum
+#Purpose = summation
+#Who ? Parameter / argument
+#install.packages("pack_name")
+
+#to use= library(pack_name)
+
+#variable creation using left-ward operator
+10
+10+2
+#efficient 
+
+num <- 10
+num
+num+100
+
+#Data types
+#1. Numeric
+
+num1<- 11
+num2 <- 10.1
+class(num1)
+class(num2)
+
+#2. Text/charecter
+text="hello chiral"
+class(text)
+
+#Naming convention
+#should be meaningful,clean and clear
+a <-30
+age <-30
+my_age <- 30
+TRUE <- 4
+?sum
+#reserve word should not be used
+?Reserved
+
+#3. logical
+
+4>2
+is_weekday <- TRUE
+class(is_weekday)
+
+# +,-,*,/ = operators
+#5(input/operand) +(operator) 2 = 7(output/operand)
+
+#1. Arithmatic operator
+
+num1 <- 10
+mum2 <- 20
+
+num1 + num2
+num1-num2
+num1*num2
+num1/num2
+num2**num1
+
+num1 %% 2
+# %% it gives remainder
+
+# 2. relational operator or,and, not
+2==2
+2!=2
+
+#3. logical operator
+
+2==2
+!2==2
